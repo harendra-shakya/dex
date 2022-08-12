5e5f71df3e9b6af3155a5205948dd209f135afd1
harendra-shakya <harendrashakya125@gmail.com>
2022-08-12 15:18:08 +0530

    updated abis in frontend

--- backend/deploy/03_Update_Frontend.ts
+++ backend/deploy/03_Update_Frontend.ts
@@ -1,34 +1,62 @@
-import { ethers } from "hardhat";
+import { DeployFunction } from "hardhat-deploy/types";
+import { ethers, network } from "hardhat";
 import * as fs from "fs";
-import { Contract } from "ethers";
 
-export default async function () {
+const deployFunction: DeployFunction = async () => {
     await updateAbi();
-    await updateContractAddresses();
-}
+    await updateAddresses();
+};
 
-const abiPath = "../../frontend/constants/";
-const mappingPath = "../../frontend/constants/networkMapping.json";
+const abiPath = "../frontend/constants/";
+const mappingPath = "../frontend/constants/networkMapping.json";
 
 const updateAbi = async () => {
     const factory = await ethers.getContract("Factory");
     const router = await ethers.getContract("Router");
-    const token = await ethers.getContract("LiquidityToken");
-    const strings = ["Factory.json", "Router.json", "LiquidityToken.json"];
-    const contracts = [factory, router, token];
-    type interfaceType = {
-        aniPath: string;
-        inter: string | string[];
-    };
+    const strings = ["Factory.json", "Router.json"];
+    const contracts = [factory, router];
 
     for (let i = 0; i < strings.length; i++) {
-        const inter = contracts[i].interface.format(ethers.utils.FormatTypes.json);
-        fs.writeFileSync(abiPath + strings[i], inter);
+        let _interface: string | NodeJS.ArrayBufferView = contracts[i].interface.format(
+            ethers.utils.FormatTypes.json
+        ) as string | NodeJS.ArrayBufferView;
+
+        fs.writeFileSync(abiPath + strings[i], _interface);
     }
 };
 
-const updateContractAddresses = async () => {
+const updateAddresses = async () => {
     const factory = await ethers.getContract("Factory");
     const router = await ethers.getContract("Router");
-    const token = await ethers.getContract("LiquidityToken");
+    const weth = await ethers.getContract("WETH");
+    const usdc = await ethers.getContract("USDC");
+    const wbtc = await ethers.getContract("WBTC");
+    const dai = await ethers.getContract("DAI");
+    const contractAddresses = await JSON.parse(fs.readFileSync(mappingPath, "utf8"));
+    const chainId = await network.config.chainId?.toString();
+
+    const strings = ["Factory", "Router", "WETH", "USDC", "WBTC", "DAI"];
+    const addresses = [
+        factory.address,
+        router.address,
+        weth.address,
+        usdc.address,
+        wbtc.address,
+        dai.address,
+    ];
+
+    for (let i = 0; i < strings.length; i++) {
+        if (chainId! in contractAddresses) {
+            if (!contractAddresses[chainId!][strings[i]]) {
+                contractAddresses[chainId!][strings[i]] = [addresses[i]];
+            }
+        } else {
+            contractAddresses[chainId!] = { [strings[i]]: [addresses[i]] };
+        }
+    }
+
+    fs.writeFileSync(mappingPath, JSON.stringify(contractAddresses));
 };
+
+export default deployFunction;
+deployFunction.tags = ["all", "frontend"];
